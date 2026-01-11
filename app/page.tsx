b07819534711cd768f36db3d84055a9f8b11980e
Sergi <sergi@grupoRegia.com>
2026-01-10 21:07:16 -0600

    Agregar pt-16 para bajar todo el contenido del hero

--- app/page.tsx
+++ app/page.tsx
@@ -435,7 +435,7 @@ export default function HomePage() {
         </header>
 
         {/* Contenido principal - flujo vertical normal */}
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 w-full max-w-6xl flex flex-col items-center justify-center px-4">
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 w-full max-w-6xl flex flex-col items-center justify-center px-4 pt-16">
           {featuredEvent ? (
             <>
               {/* Nombre del evento - Victor Mendivil (MÁS GRANDE) */}
